3da722c87697f9833e3808746be3ff2c90bf8931
ronvince <vincentroni93@gmail.com>
2018-12-20 18:19:14 +0530

    Adding id search

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser'); 
+var {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -11,6 +12,10 @@ app.use(bodyParser.json());
 const port = process.env.PORT || 3000;
 
 app.post('/todos', (req,res) =>{
+
+	if(req.body.text == null){
+		console.log('here');
+	}
 	var todo = new Todo({
 		text : req.body.text
 	});
@@ -27,7 +32,27 @@ app.get('/todos', (req, res)=>{
 	Todo.find().then((todos) =>{
 		res.send({todos});
 	}, (e) => {
-		res.status(400).send(e);
+		res.status(404).send(e);
+	});
+});
+
+
+app.get('/todos/:id', (req, res)=>{
+	var id = req.params.id;
+	console.log(id);
+	console.log(ObjectID.isValid(id));
+	if(!(ObjectID.isValid(id))){
+		return res.status(404).send(id);
+	}
+
+	Todo.findById(id).then((todo)=>{
+		if(!todo){
+			return res.status(404).send();
+		}
+		
+		res.send({todo});
+	}).catch((e) =>{
+		res.status(400).send();
 	});
 });
 
